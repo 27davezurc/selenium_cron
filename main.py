a9e4773163a887b3bd860d6cb7bf91d0a5370faf
27davezurc <90871066+27davezurc@users.noreply.github.com>
2022-02-01 10:30:13 -0800

    Update main.py

--- main.py
+++ main.py
@@ -30,12 +30,15 @@ loginBtn.click()
 element = WebDriverWait(driver, 10).until(EC.title_is("Daily Health Check"))
 print('filling the form')
 driver.find_element_by_xpath('/html/body/div/div[2]/div[3]/div[3]/div[2]/fieldset/div[2]/div[1]/button').click()
-# yes
+# yes 
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, r'/html/body/div/div[2]/div[3]/div[4]/div[2]/fieldset[1]/div[2]/div[1]/button'))).click()
 # no
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, r'/html/body/div/div[2]/div[3]/div[4]/div[2]/fieldset[2]/div[2]/div[2]/button'))).click()
 # no
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, r'/html/body/div/div[2]/div[3]/div[4]/div[2]/fieldset[3]/div[2]/div[2]/button'))).click()
+# no
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, r'/html/body/div/div[2]/div[3]/div[4]/div[2]/fieldset[4]/div[2]/div[2]/button'))).click()
+
 # confirm both
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, r'//*[@id="confirm_mask"]'))).click()
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, r'//*[@id="confirm"]'))).click()
